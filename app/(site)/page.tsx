45872babb62d92eb2385fee03ecde26472244d91
nihegde <nihegde@ucsd.edu>
2024-05-29 16:00:19 -0700

    bunch of layout changes

---
+++ app/(site)/page.tsx
@@ -0,0 +1,36 @@
+import Header from '@/components/Header';
+import ListItem from '@/components/ListItem';
+import React from 'react';
+
+export default function Home() {
+  return <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <Header><div className='mb-2'>
+      <h1 
+        className="
+          text-white
+          text-3xl
+          font-semibold">
+          Welcome Back </h1>
+
+
+          <div
+            className='
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            '
+          >
+            <ListItem
+              name='Liked Songs'
+              image='/images/liked.png'
+              href='liked'
+            />
+          </div>
+          </div>
+          </Header>
+  </div>
+}
